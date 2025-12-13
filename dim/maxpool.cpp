aac250b879999cadb3dfa7902ddab795bf51064f
jacqueline8719 <jtpham@usc.edu>
2025-12-12 22:18:21 -0800

    fix maxpool II violation with local window buffer

--- dim/maxpool.cpp
+++ dim/maxpool.cpp
@@ -26,21 +26,30 @@ void maxpool(
         for (int oh = 0; oh < H_OUT; oh++){
             OW_LOOP:
             for (int ow = 0; ow < W_OUT; ow++){
-                #pragma HLS PIPELINE II=1
-                fixed_point_t max_val = activations[oh*S][ow*S][ic];
+                
+                fixed_point_t window[3][3];
+                #pragma HLS ARRAY_PARTITION variable=window complete dim=0
+                
+                LOAD_KH:
+                for (int kh = 0; kh < K; kh++) {
+                    LOAD_KW:
+                    for (int kw = 0; kw < K; kw++) {
+                        #pragma HLS PIPELINE II=1
+                        int ih = oh * S + kh;
+                        int iw = ow * S + kw;
+                        window[kh][kw] = activations[ih][iw][ic];
+                    }
+                }
+                
+                fixed_point_t max_val = window[0][0];
                 KH_LOOP:
                 for (int kh = 0; kh < K; kh++) {
                     #pragma HLS UNROLL
                     KW_LOOP:
                     for (int kw = 0; kw < K; kw++) {
                         #pragma HLS UNROLL
-
-                        int ih = oh * S + kh;
-                        int iw = ow * S + kw;
-
-                        fixed_point_t v = activations[ih][iw][ic];
-                        if (v > max_val)
-                            max_val = v;
+                        if (window[kh][kw] > max_val)
+                            max_val = window[kh][kw];
                     }
                 }
                 output_local[oh][ow] = max_val;
